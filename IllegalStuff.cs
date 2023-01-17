381e82b9165d3f6335fa0f59cc79c0a800635d3c
Sebastian Jankowski <41026189+sjankowskim@users.noreply.github.com>
2023-01-16 21:49:49 -0500

    Add files via upload

---
+++ IllegalStuff.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace SpellRecolor
+{
+    static class IllegalStuff
+    {
+        public static T GetField<T>(this object instance, string fieldName)
+        {
+            if (instance == null) return default;
+            BindingFlags bindFlags = BindingFlags.Instance
+                                     | BindingFlags.Public
+                                     | BindingFlags.NonPublic
+                                     | BindingFlags.Static;
+            FieldInfo field = instance.GetType().GetField(fieldName, bindFlags);
+            return (T)field.GetValue(instance);
+        }
+        public static void SetField<T, U>(this T instance, string fieldName, U value)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Static;
+            FieldInfo field = instance.GetType().GetField(fieldName, bindFlags);
+            field.SetValue(instance, value);
+        }
+    }
+}
